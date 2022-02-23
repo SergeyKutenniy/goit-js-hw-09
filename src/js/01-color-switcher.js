db2de014352e392c20d7e02016f47df9cbae6411
Sergey Kutenniy <kutenniysergey@gmail.com>
2022-02-23 14:18:05 +0200

    task1-done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+const interval = {
+  intervalId: null,
+  isActive: false,
+
+  startChangeColor() {
+    if (this.isActive) {
+      return;
+    }
+    this.intervalId = setInterval(getRandomHexColor, 1000);
+    this.isActive = true;
+
+    function getRandomHexColor() {
+      return (document.body.style.backgroundColor = `#${Math.floor(
+        Math.random() * 16777215,
+      ).toString(16)}`);
+    }
+  },
+
+  stopChangeColor() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  },
+};
+
+startBtn.addEventListener('click', () => {
+  interval.startChangeColor();
+});
+stopBtn.addEventListener('click', () => {
+  interval.stopChangeColor();
+});
